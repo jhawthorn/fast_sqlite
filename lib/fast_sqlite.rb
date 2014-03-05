08594aabbe6be61ef4b6cfe924688911402498d1
John Hawthorn <john.hawthorn@gmail.com>
2014-03-05 07:07:05 -0500

    probably going to software hell for this

--- lib/fast_sqlite.rb
+++ lib/fast_sqlite.rb
@@ -1,5 +1,20 @@
 require "fast_sqlite/version"
+require 'sqlite3'
 
-module FastSqlite
-  # Your code goes here...
+SQLite3::Database.class_eval do
+  alias_method :initialize_original, :initialize
+  def initialize(*args)
+    initialize_original(*args)
+    gotta_go_fast!
+
+    if block_given?
+      yield(self)
+      close
+    end
+  end
+
+  def gotta_go_fast!
+    execute('PRAGMA synchronous = OFF')
+    execute('PRAGMA journal_mode = MEMORY')
+  end
 end
